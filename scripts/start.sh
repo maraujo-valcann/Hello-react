bf4feb07f9902f31b74e53f0a728def62c52d764
maraujo-valcann <135266102+maraujo-valcann@users.noreply.github.com>
2023-06-21 19:32:50 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 nomeApp="Hello-react"
 cd /appdir
-npm install
 npm update
-npm install resolve-url@latest
-
+npm install
 npm audit fix --force
 #sudo supervisord
 sudo supervisorctl start $nomeApp
